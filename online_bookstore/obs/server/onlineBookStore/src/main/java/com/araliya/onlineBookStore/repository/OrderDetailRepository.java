1536e4333f525f4cea8896a04f014dc5f4b7f5a7
ViyathAraliya <150628231+ViyathAraliya@users.noreply.github.com>
2024-04-02 07:45:28 +0530

    Delete online_bookstore which belongs to different repo

--- online_bookstore/obs/server/onlineBookStore/src/main/java/com/araliya/onlineBookStore/repository/OrderDetailRepository.java
+++
@@ -1,9 +0,0 @@
-package com.araliya.onlineBookStore.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import com.araliya.onlineBookStore.entity.OrderDetail;
-
-@Repository
-public interface OrderDetailRepository extends JpaRepository<OrderDetail,Long>  {}
